80e52fa100d7b5b39da429ded7dd78c1b9afc3da
wslyvh <wslyvh@users.noreply.github.com>
2022-09-28 13:44:25 +0200

    file & error handling

--- src/scripts/generate-images.ts
+++ src/scripts/generate-images.ts
@@ -1,5 +1,5 @@
 import fetch from 'cross-fetch'
-import { APP_URL } from 'utils/constants'
+import { APP_URL, GetBaseUri } from 'utils/constants'
 import { getDay } from 'utils/generator'
 import fs from 'fs'
 
@@ -11,14 +11,18 @@ async function Run() {
     const response = await fetch(`${APP_URL}api/schedule`)
     const body = await response.json()
 
-    for (let i = 0; i < body.data.length; i++) {
+    for (let i = 0; i < 3; i++) {
         const session = body.data[i]
-        console.log('PROCESSING', session.id)
 
         setTimeout(async () => {
+            console.log('PROCESSING', session.id, 'social')
             await GenerateImage(session, 'og')
+        }, i * 2000)
+
+        setTimeout(async () => {
+            console.log('PROCESSING', session.id, 'video')
             await GenerateImage(session, 'video')
-        }, i * 1000)
+        }, i * 2000)
     }
 }
 
@@ -26,9 +30,37 @@ async function GenerateImage(session: any, type: 'og' | 'video') {
     const day = getDay(session.start)
     const room = session.room.name
 
-    const res = await fetch(`https://efdevcon-api.vercel.app/api/image/${type}?id=${session.id}`)
-    const buffer = Buffer.from(await res.arrayBuffer())
-    const dirName = `./generated/${day}/${room}`
-    fs.mkdir(dirName, { recursive: true }, () => '')
-    fs.writeFile(`${dirName}/${session.id}_${type === 'og' ? 'social' : '1080'}.png`, buffer, () => '')
+    const res = await fetch(`${GetBaseUri()}api/image/${type}?id=${session.id}`)
+    if (res.status === 200) {
+        const arr = await res.arrayBuffer()
+        const png = isPng(new Uint8Array(arr))
+        const buffer = Buffer.from(arr)
+        if (png) {
+            const dirName = `./generated/${day}/${room}`
+            fs.mkdir(dirName, { recursive: true }, () => '')
+            fs.writeFile(`${dirName}/${session.id}_${type === 'og' ? 'social' : '1080'}.png`, buffer, () => '')
+        }
+        else {
+            console.warn('Invalid image file.', session.id, type)
+        }
+    }
+    else {
+        console.warn('Unable to fetch image.', session.id, type)
+        console.log(res)
+    }
+}
+
+export function isPng(buffer: Uint8Array): boolean {
+    if (!buffer || buffer.length < 8) {
+        return false
+    }
+
+    return buffer[0] === 0x89
+        && buffer[1] === 0x50
+        && buffer[2] === 0x4E
+        && buffer[3] === 0x47
+        && buffer[4] === 0x0D
+        && buffer[5] === 0x0A
+        && buffer[6] === 0x1A
+        && buffer[7] === 0x0A;
 }
