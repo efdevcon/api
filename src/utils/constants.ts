c7ab11b8f2b922c57cd5c76f8ca64685cb36967d
wslyvh <wslyvh@users.noreply.github.com>
2022-09-28 14:40:48 +0200

    config

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,12 @@
 export const SITE_URL = 'https://efdevcon-api.vercel.app/'
-export const APP_URL = 'https://app--efdevcon.netlify.app/'
+export const APP_URL = 'https://app.devcon.org/'
+
+require('dotenv').config()
 
 export function GetBaseUri() {
-    return process.env.NODE_ENV === "production" ? SITE_URL : 'http://localhost:3000/'
+    return process.env.NODE_ENV === "development" ? 'http://localhost:3000/' : SITE_URL
 }
 
 export function GetCacheControl() {
-    return process.env.NODE_ENV === "production" ? 'public, max-age=0, s-maxage=86400, stale-while-revalidate=60' : ''
+    return process.env.NODE_ENV === "development" ? '' : 'public, max-age=0, s-maxage=86400, stale-while-revalidate=60'
 }
